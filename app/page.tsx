72e5c1c2e4d26933de3949a52cadac73f85ca151
billie <seewhat1210@gmail.com>
2023-09-20 17:31:10 +0900

    add_bg

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,35 @@
+'use client'
+import { useEffect, useState } from "react";
 import BGChart from "./components/bgchart";
 import MainChart from "./components/mainchart";
 
-
 export default function Home() {
-  return (
-    <main id="wrap">
-      <div className="grap-container">
-        <div className="marker"></div>
-        <BGChart />
-        <MainChart />
+  const [isLoading, setIsLoading] = useState(true);
 
-        <div className="label-container">
-          <div className="label">0</div>
-          <div className="label">20</div>
-          <div className="label">40</div>
-          <div className="label">60</div>
-          <div className="label">80</div>
-          <div className="label">100</div>
+  useEffect(() => {
+    setIsLoading(false);
+  },[])
+
+  return (
+    <>
+      {!isLoading && (
+        <main id="wrap">
+        <div className="grap-container">
+          <div className="marker"></div>
+          <BGChart />
+          <MainChart />
+  
+          <div className="label-container">
+            <div className="label">0</div>
+            <div className="label">20</div>
+            <div className="label">40</div>
+            <div className="label">60</div>
+            <div className="label">80</div>
+            <div className="label">100</div>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+      )}
+    </>
   )
 }
